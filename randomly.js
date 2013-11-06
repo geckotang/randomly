900145a835737e4cbb9171aa983d4e6905ad4e98
geckotang <macky1207@gmail.com>
2013-11-06 18:05:48 +0900

    ひな形作成

--- randomly.js
+++ randomly.js
@@ -1,12 +1,38 @@
-/*! randomly.js ()
+/*! randomly.js (git@github.com:geckotang/randomly.git)
  * lastupdate: 2013-11-06
  * version: 0.0.0
  * author: @geckotang
  * License: MIT */
-(function(){
-	console.log(0);
-	console.log(0);
-	console.log(0);
-	console.log(0);
-	console.log(0);
-}());
+(function(window, document, undefined){
+	"use strict";
+
+	function Randomly(element, opts) {
+		return (this instanceof Randomly)
+			? this.init(array)
+			: new Randomly(array);
+	};
+
+	Randomly.prototype.init = function(array) {
+		var that = this;
+		that.items = array;
+		return that;
+	};
+
+	// 配列から無作為にnumの数だけ抽出する
+	// @param num {Number} 抽出したい数
+	// @return result {Object} 該当したもの(result.matched)と、それ以外(result.unmached)をオブジェクトで返す。
+	Randomly.prototype.pickout = function(num) {
+		var that = this;
+		var items = that.items.concat();
+		var mached = [];
+		var unmached = [];
+
+		return {
+			mached: mached,
+			unmached: unmached
+		};
+	};
+
+	window.Randomly = Randomly;
+
+}(window, window.document));
--- src/randomly.js
+++ src/randomly.js
@@ -1,7 +1,33 @@
-(function(){
-	console.log(0);
-	console.log(0);
-	console.log(0);
-	console.log(0);
-	console.log(0);
-}());
+(function(window, document, undefined){
+	"use strict";
+
+	function Randomly(element, opts) {
+		return (this instanceof Randomly)
+			? this.init(array)
+			: new Randomly(array);
+	};
+
+	Randomly.prototype.init = function(array) {
+		var that = this;
+		that.items = array;
+		return that;
+	};
+
+	// 配列から無作為にnumの数だけ抽出する
+	// @param num {Number} 抽出したい数
+	// @return result {Object} 該当したもの(result.matched)と、それ以外(result.unmached)をオブジェクトで返す。
+	Randomly.prototype.pickout = function(num) {
+		var that = this;
+		var items = that.items.concat();
+		var mached = [];
+		var unmached = [];
+
+		return {
+			mached: mached,
+			unmached: unmached
+		};
+	};
+
+	window.Randomly = Randomly;
+
+}(window, window.document));
